6e020961bbe034ec7c8105e0a88131e2ab6015ef
DuduGuima <eduardo.guima.dudu@gmail.com>
2024-05-11 17:37:47 +0200

    ASF 90% acc

--- script_tp2.py
+++ script_tp2.py
@@ -1,7 +1,10 @@
 import numpy as np
-from skimage.morphology import erosion, dilation, binary_erosion, opening, closing, white_tophat, reconstruction, black_tophat, skeletonize, convex_hull_image, thin,area_opening, area_closing
+from skimage.morphology import local_minima,local_maxima,erosion, dilation, binary_erosion, opening, closing, white_tophat, reconstruction, black_tophat, skeletonize, convex_hull_image, thin,area_opening, binary_closing,binary_opening
 from skimage.morphology import square, diamond, octagon, rectangle, star, disk
-from skimage.filters.rank import entropy, enhance_contrast_percentile
+from skimage.filters.rank import entropy, enhance_contrast_percentile,mean,gradient_percentile,gradient
+from skimage.filters import apply_hysteresis_threshold,laplace,threshold_otsu
+from skimage.segmentation import watershed
+from skimage import feature
 from PIL import Image
 from scipy import ndimage as ndi
 from skimage.util import img_as_ubyte
@@ -11,31 +14,41 @@ from matplotlib import pyplot as plt
 
 from sklearn.cluster import KMeans
 
-def segmentation_blacktophat (img, img_mask):
-    img_use = (img_mask * img)
-    #ouveerture pour efface les petits pixels blancs
-    img_use = opening(img_use,footprint=disk(12))
-    img_out = black_tophat(img_use,footprint=diamond(3))
 
-    return img_out
+def distance_approach(img,img_mask):
+    im_result = np.empty(np.shape(img))
+    im_result=img
+    im_result = gradient(im_result,disk(2))
+    im_result = ndi.gaussian_filter(im_result,3)
+    #im_result = white_tophat(im_result,disk(3))
+    #im_result = feature.canny(im_result,sigma=0.1)
+    #img_out = img_out <15
+    #entropy(img,footprint=disk(4),out=im_result)
+    # img_out=opening(im_result,footprint=disk(1),out=im_result)
+    # img_out=closing(im_result,footprint=disk(1),out=im_result)
+    # for i in range(2,4):
+    #     print('size of disk used', i)
+    #     opening(im_result,footprint=disk(i),out=im_result)
+    #     closing(im_result,footprint=disk(i),out=im_result)
+    # img_out = dilation(im_result,footprint=disk(1)) - erosion(im_result,footprint=disk(1))
+    # seuil = threshold_otsu(img_out)
+    # img_out = img_out < seuil
+    # img_out = img_mask * (img_out < 20) 
+    # img_out = binary_opening(img_out, footprint=disk(1))
+    #img_out = binary_erosion(img_out , footprint=disk(1))
+    # #img_out = ndi.distance_transform_edt(img_out)
+    return im_result
 
-def seg_ASF(img,img_mask):
+def seg_ASF(img,img_mask):#90% de acc ta aqui
     img_use = img*img_mask
-    img_out = dilation(img_use,footprint=diamond(5)) - erosion(img_use,footprint=diamond(5))
-    #img_out = opening(img_out,footprint=diamond(4))
-    img_out = erosion(img_out, footprint = diamond(6))
-    img_out = white_tophat(img_out,footprint=diamond(2))
-    img_out = area_opening(img_out,area_threshold=15)
-    #img_out = img_out > 22
-    
+    img_out = ndi.gaussian_filter(img_use,sigma=2.0) #smoothing, taking out some of the
+    #useless bright spots by enhancing the image's scale
+    img_out = black_tophat(img_out,disk(2))#blood vessels are always black, so we get them
+    img_out = mean(img_out,disk(3))#erase small bright spots in the background
+    img_out = opening(img_out, disk(2))#again, to erase bright spots and smooth out image
     return img_out
 
-def segmentation_kmeans(img,img_mask,seuil,n_composantes=2):
-    img_seuil = img * img_mask
-    img_train = np.reshape(img_seuil,(-1,1))
-    kmeans_model = KMeans(n_clusters=n_composantes,random_state=0).fit(img_train)
-    img_labels = np.reshape(kmeans_model.labels_,np.shape(img))
-    return img_labels
+
 
 def my_segmentation(img, img_mask, seuil):
     img_out = (img_mask & (img < seuil))
@@ -74,11 +87,10 @@ img_mask[invalid_pixels] = 0
 
 
 img_out_prof = my_segmentation(img,img_mask,80)
-img_out_kmeans = segmentation_kmeans(img,img_mask,80,n_composantes)
-img_out_blacktophat = segmentation_blacktophat(img,img_mask)
+img_out_distance = distance_approach(img,img_mask)
 img_out_ASF = seg_ASF(img,img_mask)
 print(np.sum(img_out_prof == 1)/(512*512))
-print(np.sum(img_out_blacktophat == 1)/(512*512))
+print(np.sum(img_out_distance== 1)/(512*512))
 #here only a few values of img are going to remain, based on the 
 #invalid pixels and seuil used to 'filter' 
 
@@ -88,15 +100,12 @@ img_GT =  np.asarray(Image.open('images_IOSTAR/GT_01.png')).astype(np.bool_)
 
 
 ACCU, RECALL, img_out_skel, GT_skel = evaluate(img_out_prof, img_GT)
-ACCU_kmeans,RECALL_kmeans,img_skel_kmeans,_ = evaluate(img_out_kmeans,img_GT)
-ACCU_btophat,RECALL_btophat,img_skel_btophat,_ = evaluate(img_out_blacktophat,img_GT)
+ACCU_distance,RECALL_distance,img_skel_distance,_ = evaluate(img_out_distance,img_GT)
 ACCU_ASF,RECALL_ASF,img_skel_ASF,_ = evaluate(img_out_ASF,img_GT)
 
 print('Accuracy =', ACCU,', Recall =', RECALL)
-#print('Accuracy kmeans =', ACCU_kmeans,', Recall kmeans =', RECALL_kmeans)
-print('Accuracy btophat =', ACCU_btophat,', Recall btophat =', RECALL_btophat)
+print('Acc distance = ',ACCU_distance, 'Recall Dist : ',RECALL_distance)
 print('Accuracy ASF =', ACCU_ASF,', Recall ASF =', RECALL_ASF)
-
 
 plt.subplot(231)
 plt.imshow(img,cmap = 'gray')
@@ -127,8 +136,8 @@ fig.tight_layout()
 
 #axs[2].set_title('Black Top Hat')
 axs[0][0].imshow(img*img_mask, cmap='gray')
-axs[0][1].imshow(img_out_blacktophat, cmap='gray')
-axs[0][2].imshow(img_skel_btophat,cmap = 'gray')
+axs[0][1].imshow(img_out_distance, cmap='gray')
+axs[0][2].imshow(img_skel_distance,cmap = 'gray')
 
 axs[1][0].imshow(img*img_mask, cmap='gray')
 axs[1][1].imshow(img_out_ASF, cmap='gray')
